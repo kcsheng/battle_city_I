e190231cf244f5b6aab2d180f3cae8f986125028
kcsheng <kacysheng@mail.com>
2020-05-28 09:42:55 +1000

    refactor the movement code

--- src/tank.rb
+++ src/tank.rb
@@ -7,42 +7,35 @@ class Tank
         @tank_south = Gosu::Image.new("../media/blue_tank_south.png")
         @x = 260
         @y = 644 
-        @mov_west, @mov_east, @mov_north, @mov_south = false, false, false, false
-        @left, @right, @top, @bottom = @x, @x + 56, @y, @y + 56
+        @mov_west, @mov_east, @mov_north, @mov_south = [false] * 4
     end
 
-    # def move        
-    #     if @mov_west
-    #         @x -= 1 
-    #         if @left < 0   
-    #             @x += 5
-    #         end
-    #     end
-        
-    #     if @mov_east 
-    #         @x += 1 
-    #         if @right > 800
-    #             @x -= 5
-    #         end
-    #     end
+    def move_west        
+        @mov_west, @mov_east, @mov_north, @mov_south = true, false, false, false
+        @x -= 1
+        @x += 5 if @x < 0 
+    end
 
-    #     if @mov_north             
-    #         @y -= 1 
-    #         if @top < 0
-    #             @y += 5
-    #         end
-    #     end
+    def move_east       
+        @mov_west, @mov_east, @mov_north, @mov_south = false, true, false, false
+        @x += 1  
+        @x -= 5 if @x + 56 > 800    
+    end
+    
+    def move_north       
+        @mov_west, @mov_east, @mov_north, @mov_south = false, false, true, false
+        @y -= 1
+        @y += 5 if @y < 0   
+    end
 
-    #     if @mov_south 
-    #         @y += 1 
-    #         if @bottom > 710
-    #             @y -= 5
-    #         end
-    #     end
-    # end
+    def move_south       
+        @mov_west, @mov_east, @mov_north, @mov_south = false, false, false, true
+        @y += 1 
+        @y -= 5 if @y + 56 > 710      
+    end
 
     def update
-        move
+        move_south
     end
 
     def draw
