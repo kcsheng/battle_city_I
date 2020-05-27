dee4a0fe7db4d38e6d2ad007f36995e126c555a1
kcsheng <kacysheng@mail.com>
2020-05-27 20:08:22 +1000

    button down close window

--- src/window.rb
+++ src/window.rb
@@ -1,5 +1,14 @@
 class GameWindow < Gosu::Window
     def initialize
         super(800, 710, false)
+        self.caption = "Battle City I"
+    end
+
+    def button_down(id)
+        if id == Gosu::KB_ESCAPE
+            close
+        else
+          super
+        end
     end
 end
