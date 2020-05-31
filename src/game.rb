d2a1d4ef3fb691853df2d68a44a513750e0d5ed1
kcsheng <kacysheng@mail.com>
2020-05-31 10:45:20 +1000

    game rb require order changed for correct initialization

--- src/game.rb
+++ src/game.rb
@@ -1,8 +1,8 @@
 require "gosu"
 require_relative "window"
 require_relative "tank"
-require_relative "player"
 require_relative "enemy_tank"
 require_relative "enemy_team"
+require_relative "player"
 require_relative "cannon"
 GameWindow.new.show
